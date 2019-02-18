89ed24e820db332222813e2c801e3b3a104958fb
Daniel Knecht <daniel.knecht@sap.com>
2019-02-18 09:41:17 +0100

    Add Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,48 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Button from "@material-ui/core/Button";
+import { AppBar, Toolbar, IconButton, Typography, createStyles, WithStyles, withStyles } from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
 
-class App extends Component {
-	render() {
-		return (
-			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<p>
-						Change <code>src/App.tsx</code> and save to reload.
-					</p>
-				</header>
-			</div>
-		);
-	}
+const styles = createStyles({
+  root: {
+    flexGrow: 1
+  },
+  grow: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20
+  }
+});
+
+export interface Props extends WithStyles<typeof styles> {}
+
+function App(props: Props) {
+  const { classes } = props;
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <div className="App">
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" color="inherit" className={classes.grow}>
+              News
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+        <Button variant="contained" color="primary">
+          Hello There
+        </Button>
+      </div>
+    </React.Fragment>
+  );
 }
-
-export default App;
+export default withStyles(styles)(App);
